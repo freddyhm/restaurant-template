db1dfdb013254544a9ad0eddeebe718aa506434f
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2015-04-22 21:12:42 -0700

    Outline structure and layout of the site as per BEM naming convention. Abstract css blocks with own directory and files. This feels like overkill for such a simple site but it's more so about the practice than the utility. Project is too small to abstract elements to their own file/folder like BEM suggests so stop at blocks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,15 +4,14 @@ module.exports = function(grunt){
 		pkg: grunt.file.readJSON('package.json'),
 
 		concat:{
+			css: {
+		    	src: 'src/**/*.css',
+		    	dest: 'dist/css/concat.css'
+		  	}
 
 		}
-
-
-
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.registerTask('default', ['concat']);
-
-	
 };
